8b40d88680e7f831d5c357331e293d5858907b87
nata <natakravchenko.nk@gmail.com>
2014-06-27 13:10:10 +0300

    add pagination for cycle table;
    change style;
    update js - level;

---
+++ journal-webapp/src/main/webapp/WEB-INF/resources/js/lib/models/cycles.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('#cyclesTable').dataTable({ "pagingType": "full_numbers" });
+});
+
+
+function editCycle(id, name) {
+    $("#cycleId").val(id);
+    $("#cycleNameId").val(name);
+}
