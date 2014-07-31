3a243f270c30fd1f77402852af4f3405419a4f7a
Powerofenergy <oknrenamor@gmail.com>
2014-07-31 22:35:04 +0300

    update teachers-dao:
    add possibility to add/delete/get teacher

--- journal-webapp/src/main/java/com/system/edu/web/dao/TeachersDao.java
+++ journal-webapp/src/main/java/com/system/edu/web/dao/TeachersDao.java
@@ -1,6 +1,10 @@
 package com.system.edu.web.dao;
 
 import com.system.edu.models.dao.TeachersEntity;
+import com.system.edu.models.dao.UsersEntity;
+import com.system.edu.models.ui.Teachers;
+import net.sf.brunneng.jom.IMergingContext;
+import net.sf.brunneng.jom.MergingContext;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +20,52 @@ import java.util.List;
 @Repository
 public class TeachersDao {
 
+    IMergingContext context = new MergingContext();
+
     @Autowired
     SessionFactory sessionFactory;
+    @Transactional
+    public boolean addTeachers(Teachers teachers) {
+        try {
+            TeachersEntity teachersEntity = context.map(teachers, TeachersEntity.class);
+            sessionFactory.getCurrentSession().merge(teachersEntity);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    @Transactional
+    public boolean checkTeachersName(String surname) {
+
+        TeachersEntity teachersEntity = (TeachersEntity) sessionFactory.getCurrentSession().createCriteria(TeachersEntity.class).add(Restrictions.eq("surname", surname))
+                .uniqueResult();
+        return teachersEntity == null ? true : false;
+    }
+
+    @Transactional
+    public void updateTeachers(Teachers teachers) {
+        TeachersEntity teachersEntity = context.map(teachers, TeachersEntity.class);
+        sessionFactory.getCurrentSession().merge(teachersEntity);
+    }
+
+    @Transactional
+    public Teachers getTeachers(int id) {
+        Object object = sessionFactory.getCurrentSession().get(UsersEntity.class, id);
+        Teachers teachers = context.map(object, Teachers.class);
+        return teachers;
+    }
+
+    @Transactional
+    public boolean deleteTeachers(int id) {
+        try {
+            TeachersEntity entityForDelete = (TeachersEntity) sessionFactory.getCurrentSession().createCriteria(TeachersEntity.class).add(Restrictions.eq("id", id)).uniqueResult();
+            sessionFactory.getCurrentSession().delete(entityForDelete);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
 
     @Transactional
     public List<TeachersEntity> getAllTeachers() {
@@ -33,4 +81,5 @@ public class TeachersDao {
                 teachersEntity.getFirstname() + " " +
                 teachersEntity.getMiddlename();
     }
+
 }
