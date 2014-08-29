80c61aab1a6aee14642e74bcdb1b7f52d22a70b1
nata <natalia.kravchenko@google.com>
2014-08-29 22:48:10 +0300

    sketched delete confirmation fo cycle page

--- journal-webapp/src/main/webapp/WEB-INF/resources/js/lib/models/cycles.js
+++ journal-webapp/src/main/webapp/WEB-INF/resources/js/lib/models/cycles.js
@@ -1,9 +1,14 @@
 $(document).ready(function () {
     $('#cyclesTable').dataTable({ "pagingType": "full_numbers" });
+    $('#cyclesTable').dataTable({ "pagingType": "full_numbers" });
 });
 
 
 function editCycle(id, name) {
     $("#cycleId").val(id);
     $("#cycleNameId").val(name);
+}
+
+function deleteCycleById(cycleId) {
+    alert(cycleId);
 }
